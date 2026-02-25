00d900e8d8dcad82c931e1d98c6335712ab60cd2
arkostaa <arkosta13@gmail.com>
2026-02-25 15:20:51 +0300

    update

--- service_a/app.py
+++ service_a/app.py
@@ -1,19 +1,27 @@
-import time
 import requests
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    time.sleep(0.1)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    message = ""
+    # POST запрос
+    if request.method == "POST":
+        vote = request.form.get("vote")
+        try:
+            requests.post(f"http://service_b:5000/vote", json={"tech": vote})
+            message = f"Спасибо! Ваш голос за {vote} принят."
+        except:
+            message = "Ошибка на внутренней стороне."
+
     try:
-        # Обращаемся к service_b по имени контейнера
-        response = requests.get("http://service_b:5000/calculate")
-        result = response.text
+        r = requests.get("http://service_b:5000/results")
+        data = r.json()
     except:
-        result = "Service B unavailable"
-    return f"Service A says: {result}"
+        data = {"python": 0, "go": 0}
+
+    return render_template("index.html", data=data, message=message)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- service_b/app.py
+++ service_b/app.py
@@ -1,14 +1,32 @@
 import time
-import random
-from flask import Flask
+import redis
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/calculate")
-def calculate():
-    delay = random.uniform(0.1, 0.5)
-    time.sleep(delay)
-    return f"Calculation done in {delay:.2f} seconds!"
+r = redis.Redis(host='redis', port=6379, db=0)
+
+@app.route("/vote", methods=["POST"])
+def vote():
+    time.sleep(0.1)
+    
+    data = request.json
+    tech = data.get("tech")
+    
+    if tech in ["python", "go"]:
+        r.incr(tech)
+        return jsonify({"status": "ok", "voted_for": tech})
+    return jsonify({"error": "invalid choice"}), 400
+
+@app.route("/results", methods=["GET"])
+def results():
+    python_votes = r.get("python") or 0
+    go_votes = r.get("go") or 0
+    
+    return jsonify({
+        "python": int(python_votes),
+        "go": int(go_votes)
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
